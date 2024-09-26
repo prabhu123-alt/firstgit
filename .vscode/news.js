430e9f9183944d0566899eb2597b4fa68ab89052
w3schools-test <bhavesh.singh@teknopoint.in>
2024-09-26 18:45:27 +0530

    fill is updeting

--- .vscode/news.js
+++ .vscode/news.js
@@ -1,20 +1,23 @@
 const URL = "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME";
+
 const api = async () => {
     try {
         let response = await fetch(URL);
-        let data = await response.json();
+        let data = await response.json();     
         console.log(data);
-        data.forEach(element => {
-            let allImages = document.querySelectorAll(".image2");
-            let alltitels = document.querySelectorAll(".titel");
-            allImages.forEach(img => {
-                img.src = element.url; 
-            });
-            alltitels.forEach(title =>{
-                title.innerHTML = element.id;
-            });
+        let allImages = document.querySelectorAll(".image2");
+        let title = document.querySelectorAll(".titel");
+        data.forEach((element, index) => {
+            if (allImages[index]) {
+                allImages[index].src = element.url; 
+            }
+            if (title[index]) {
+                title[index].textContent = element.id;
+            }
         });
     } catch (error) {
-        console.log("error", error)
+        console.log("error", error);
     }
-}; api();
+};
+
+api();
